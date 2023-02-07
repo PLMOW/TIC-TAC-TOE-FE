7dd8e576a461000459b4c47891989c4727b81ff2
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-07 17:16:00 +0900

    chore: 폴더명 수정 및 불필요한 import 삭제

--- src/layouts/search/index.tsx
+++ src/layouts/search/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { searchDataAtom } from '../../atom';
 import styled from 'styled-components';
