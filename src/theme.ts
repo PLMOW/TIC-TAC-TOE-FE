134a5cd3e86f39def25e350cc09ce5ff9a4376ed
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-07 17:52:17 +0900

    feat: Dark/Light theme Card 색상 추가

--- src/theme.ts
+++ src/theme.ts
@@ -3,9 +3,13 @@ import { DefaultTheme } from 'styled-components';
 export const darkTheme: DefaultTheme = Object.freeze({
   text: 'whitesmoke',
   background: '#2b2b2b',
+  cardText: '#cbd5e1',
+  cardBackground: '#111827',
 });
 
 export const lightTheme: DefaultTheme = Object.freeze({
   text: '#2b2b2b',
   background: 'white',
+  cardText: '#111827',
+  cardBackground: '#cbd5e1',
 });
