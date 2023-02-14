dc585d94ce98d27246e59a2b04c398140946eb6d
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-14 18:09:26 +0900

    feat: iconBackground Color 색상 및 type 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,11 @@
 import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = Object.freeze({
-  text: 'whitesmoke',
+  text: '#cbd5e1',
   background: '#2b2b2b',
   cardText: '#cbd5e1',
   cardBackground: 'rgba(255, 255, 255, 0.2)',
+  iconBackground: 'rgba(222,222,222,0.5)',
 });
 
 export const lightTheme: DefaultTheme = Object.freeze({
@@ -12,4 +13,5 @@ export const lightTheme: DefaultTheme = Object.freeze({
   background: '#cbd5e1',
   cardText: '#cbd5e1',
   cardBackground: 'rgba(255, 255, 255, 0.2)',
+  iconBackground: 'rgba(222,222,222,0.5)',
 });
